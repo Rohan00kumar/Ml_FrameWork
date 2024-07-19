32cca082fb5270f6d2a3eeede404866406ae1709
Rohan Kumar <113842997+Rohan00kumar@users.noreply.github.com>
2024-07-19 20:55:09 +0530

    Create data_loader.py

---
+++ data/data_loader.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+class DataLoader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load_data(self):
+        data = pd.read_csv(self.file_path)
+        X = data.drop('target', axis=1)
+        y = data['target']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        return (X_train, y_train), (X_test, y_test)
